c161d3b509b847912b6fca4999570863ccdefd19
omri <omrikofialkov@gmail.com>
2025-01-22 14:08:35 +0200

    working terraform ec2 docker compose project - final phase 4

--- main.tf
+++ main.tf
@@ -66,6 +66,7 @@ resource "aws_instance" "flask_instance" {
               systemctl enable docker
               usermod -aG docker ec2-user
               newgrp docker
+              yum install -y libxcrypt-compat
               curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
               chmod +x /usr/local/bin/docker-compose
               git clone https://github.com/OmriFialkov/flask-catexer-app-actions.git /home/ec2-user/flask-app
