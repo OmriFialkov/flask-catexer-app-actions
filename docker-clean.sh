29a3d5cb362ec3f77aaa03ab49621f92065585e4
omri <omrikofialkov@gmail.com>
2025-02-06 18:26:59 +0200

    adding and testing docker hub tags cleanup script for older tags.

--- docker-clean.sh
+++ docker-clean.sh
@@ -4,53 +4,78 @@ echo """
 this script will remove all docker tags older than 7 days
 from docker hub repo of this project
 usage: ./docker-clean.sh
+
 starting..
 """
+
+# this variables are secrets fetched from github actions' runner environment.
+docker_user="${DOCKER_USER}"
+docker_repo="${DOCKER_REPO}"
+docker_token="${DOCKERTOKEN}"
+
 
 sleep 2
 
 #____________________________________________________________________________________________________
 
+echo ""
 echo "fetching current tags in docker hub repo and showing them"
 
+# accessing docker hub api to fetch all current tags and using jq to format them to my choice.
 tags=$(curl -s -H "Authorization: Bearer $docker_token" \
     "https://hub.docker.com/v2/repositories/$docker_user/$docker_repo/tags/?page_size=100" \
-    | jq '.results[] | {name: .name, created_at: (.tag_last_pushed | split("T")[0] | gsub("-"; "."))}')
+    | jq '[.results[] | {name: .name, created_at: (.tag_last_pushed | split("T")[0])}]')
 
 echo "$tags"
 sleep 2
 
 #____________________________________________________________________________________________________
 
+echo ""
 echo "comparing dates and calculating outdated tags out of tags shown.."
-sleep 1
 
-today_minus_week=$(date -d "-7 days" "+%Y.%m.%d")
-echo "shortly tags which were created before $today_minus_week will be deleted from the docker hub repo"
+# calculating today minus 7 days using date -d
+today_minus_week=$(date -d "-7 days" "+%s")
+echo ""
+echo "shortly tags which were created before $(date -d "-7 days") will be deleted from the docker hub repo"
+echo ""
 
-for tag in $tags; do
-    echo "checking tag $tag date"
-    tag_date_only=$(echo $tag | jq -r '.created_at')
-    if [[ $tag_date_only -lt $today_minus_week ]]; then
+# using jq to iterate over tags with a while loop that reads tag in a line each iteration.
+echo "$tags" | jq -c '.[]' | while IFS= read -r tag_object; do
+
+    tag=$(echo "$tag_object" | jq -r '.name')
+    tag_date_only=$(echo "$tag_object" | jq -r '.created_at')
+
+    echo "checking tag $tag with date $tag_date_only"
+
+    # comparing tag date with today minus 7 days with unix timestamp ("+%s")
+    if [[ "$(date -d "$tag_date_only" "+%s")" -lt "$today_minus_week" ]]; then
         echo "removing tag $tag"
         curl -X DELETE -H "Authorization: Bearer $docker_token" \
-            "https://hub.docker.com/v2/repositories/$docker_user/$docker_repo/tags/$tag"
-        echo "tag $tag was deleted!!!!!!!!!!!!!!!!!!!!!!!!!"
+            "https://hub.docker.com/v2/repositories/$docker_user/$docker_repo/tags/$tag/"
+        echo "tag $tag was deleted!!!!!!!!!"
+        echo ""
+        sleep 1
     else
-        echo "tag $tag is not outdated, keeping him"
+        echo "tag $tag is not outdated, keeping it"
+        echo ""
+        sleep 1
     fi
 done
+
 echo "deleted outdated tags, now showing remaining tags"
-sleep 1
+echo ""
 #____________________________________________________________________________________________________
 
-echo "remaining tags:"
-
-remaining_tags=$( curl -s "https://hub.docker.com/v2/repositories/crazyguy888/catexer-actions/tags" \
- | jq '.results[].name')
+# showing remaining tags after deleting outdated tags
+remaining_tags=$( curl -s -H "Authorization: Bearer $docker_token" \
+    "https://hub.docker.com/v2/repositories/crazyguy888/catexer-actions/tags/?page_size=100" \
+    | jq '.results[].name')
 
 echo "$remaining_tags"
 
-echo "done, now exiting.."
-sleep 1
-
+echo ""
+echo "docker hub tag-cleanup completed, now exiting.."
+echo "docker hub tag-cleanup completed, now exiting.."
+echo "docker hub tag-cleanup completed, now exiting.."
+echo ""
--- google-k8s/GCP-k8s-infra.tf
+++ google-k8s/GCP-k8s-infra.tf
@@ -69,11 +69,6 @@ resource "google_container_node_pool" "primary_nodes" {
   location = var.GCP_ZONE
   node_count = 1
 
-  autoscaling {
-    min_node_count = 1
-    max_node_count = 1
-  }
-
   node_config {
     machine_type = "e2-medium"
     disk_size_gb = 15
