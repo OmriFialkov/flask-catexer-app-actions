f2f5b24267ce0f7812689c885c5c62d569a683a7
omri <omrikofialkov@gmail.com>
2025-02-27 15:03:02 +0200

    hey

--- google-k8s/GCP-k8s-infra.tf
+++ google-k8s/GCP-k8s-infra.tf
@@ -67,7 +67,7 @@ resource "google_container_node_pool" "primary_nodes" {
   name     = "omri-node-pool"
   cluster  = google_container_cluster.primary.name
   location = var.GCP_ZONE
-  node_count = 1
+  node_count = 2
 
   node_config {
     machine_type = "e2-medium"
