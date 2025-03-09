a400cc319d864e98f2c22635023590313b28c227
omri <omrikofialkov@gmail.com>
2025-03-09 18:18:53 +0200

    i love argocd

--- cleanups/docker-clean.sh
+++ cleanups/docker-clean.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-echo """
-this script will remove all docker tags older than 7 days
-from docker hub repo of this project
-usage: ./docker-clean.sh
+# CHANGE FOR YOUR OWN CHOICE OF DAYS CUTOFF
+RETENTION_DAYS=7
+#___________________________________________________________________________________________________
 
-starting..
+echo """
+this script will remove all docker tags older than "$RETENTION_DAYS" days
+from docker hub repo of this project
 """
 
 # this variables are secrets fetched from github actions' runner environment..
 docker_user="${DOCKER_USER}"
 docker_repo="${DOCKER_REPO}"
 docker_token="${DOCKERTOKEN}"
-
 
 sleep 2
 
@@ -21,7 +21,7 @@ sleep 2
 echo ""
 echo "fetching current tags in docker hub repo and showing them"
 
-# accessing docker hub api to fetch all current tags and using jq to format them to my choice.
+# accessing docker-hub api to fetch all current tags and using jq to format them to my choice.
 tags=$(curl -s -H "Authorization: Bearer $docker_token" \
     "https://hub.docker.com/v2/repositories/$docker_user/$docker_repo/tags/?page_size=100" \
     | jq '[.results[] | {name: .name, created_at: (.tag_last_pushed | split("T")[0])}]')
@@ -34,10 +34,10 @@ sleep 2
 echo ""
 echo "comparing dates and calculating outdated tags out of tags shown.."
 
-# calculating today minus 7 days using date -d
-today_minus_week=$(date -d "-7 days" "+%s")
+# calculating today minus retention days using date -d
+cutoff_date=$(date -d "-$RETENTION_DAYS days" "+%s")
 echo ""
-echo "shortly tags which were created before $(date -d "-7 days") will be deleted from the docker hub repo"
+echo "Shortly, tags which were created before $(date -d "-$RETENTION_DAYS days") will be deleted from the docker hub repo"
 echo ""
 
 # using jq to iterate over tags with a while loop that reads tag in a line each iteration.
@@ -49,7 +49,7 @@ echo "$tags" | jq -c '.[]' | while IFS= read -r tag_object; do
     echo "checking tag $tag with date $tag_date_only"
 
     # comparing tag date with today minus 7 days with unix timestamp ("+%s")
-    if [[ "$(date -d "$tag_date_only" "+%s")" -lt "$today_minus_week" ]]; then
+    if [[ "$(date -d "$tag_date_only" "+%s")" -lt "$cutoff_date" ]]; then
         echo "removing tag $tag"
         curl -X DELETE -H "Authorization: Bearer $docker_token" \
             "https://hub.docker.com/v2/repositories/$docker_user/$docker_repo/tags/$tag/"
@@ -68,13 +68,11 @@ echo ""
 
 # showing remaining tags after deleting outdated tags
 remaining_tags=$( curl -s -H "Authorization: Bearer $docker_token" \
-    "https://hub.docker.com/v2/repositories/crazyguy888/catexer-actions/tags/?page_size=100" \
+    "https://hub.docker.com/v2/repositories/$docker_user/$docker_repo/tags/?page_size=100" \
     | jq '.results[].name')
 
 echo "$remaining_tags"
 
 echo ""
-echo "docker hub tag-cleanup completed, now exiting.."
-echo "docker hub tag-cleanup completed, now exiting.."
 echo "docker hub tag-cleanup completed, now exiting.."
 echo ""
